dfba3cc5e943063fe07387050813ca5ae4c62f09
Reno Kriz <rekriz@seas.upenn.edu>
2019-06-06 23:23:45 -0700

    Fix

--- human_evaluation/quality_hit/format_input_camera_ready.py
+++ human_evaluation/quality_hit/format_input_camera_ready.py
@@ -24,9 +24,8 @@ def flatten(listoflists):
 ## Detokenize and fix weird contractions
 def fix(listy, detokenize):
     for i in range(len(listy)):
-        if listy[i] == 'i' and i != 0:
-            listy[i] = "I"
-            print(listy)
+        if listy[i] == 'i' or (i > 0 and listy[i-1] in [".", "?", "!"]):
+            listy[i] = listy[i].capitalize()
             
     detok = detokenize(listy)
     fixed = str(detok)
